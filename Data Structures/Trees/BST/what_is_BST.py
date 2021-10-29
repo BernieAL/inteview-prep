83004c087778758bce99744fa4339b9d28847742
BernieAL <balmanzar883@gmail.com>
2021-10-29 16:37:55 -0400

    initial commit

---
+++ Data Structures/Trees/BST/what_is_BST.py
@@ -0,0 +1,19 @@
+"""
+
+WHAT IS IT?
+
+Its a tree that maintains the property that the value in each node must be greater than 
+or equal to any value stored in the left sub-tree and less than or equal to any value stored in the right subtree
+
+Eveything on left is less than root and right subtree
+Evetything on right is greater than root and left subtree
+
+Time Complexity:
+    Access: O(log(n))
+    Search: O(log(n))
+    Insert: O(log(n))
+    Remove: O(log(n))
+
+"""
+
+
